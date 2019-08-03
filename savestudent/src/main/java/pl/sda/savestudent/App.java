5ae63bebe11db8aaca002fee3a24ace556627711
Ryszard Kozakiewicz <ryszard.ko1111@gmail.com>
2019-08-03 11:18:07 +0200

    added modules and student list

---
+++ savestudent/src/main/java/pl/sda/savestudent/App.java
@@ -0,0 +1,47 @@
+package pl.sda.savestudent;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.sda.studentmodel.Student;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        List<Student> studentList = new ArrayList<Student>();
+
+        studentList.add(new Student("Jan", "Kowalskie", 1));
+        studentList.add(new Student("Katarzyna", "Nowak", 1));
+        studentList.add(new Student("Tomasz", "Nowicki", 1));
+        studentList.add(new Student("Jacek", "Gyros", 1));
+        studentList.add(new Student("Edi", "Petarda", 1));
+
+
+        writeJSONFile(studentList);
+
+    }
+
+
+
+    public static void writeJSONFile(List<Student> studentList){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            objectMapper.writeValue(new File("studentList.json"), studentList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
---
+++ studentmodel/src/main/java/pl/sda/studentmodel/Student.java
@@ -0,0 +1,15 @@
+package pl.sda.studentmodel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private String name;
+    private String lastname;
+    private int index;
+
+
+}
